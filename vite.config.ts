b4db5c5c1e9b0b1087b6ddf48d52c72f8b2c08c6
Tofan <tovan.kamil@gmail.com>
2025-03-14 02:03:31 +0700

    update github pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true, // Enable global test functions (describe, it, expect, etc.)
   },
   plugins: [react(), tailwindcss(), Inspect()],
-
+  base: "/github-user-search",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
