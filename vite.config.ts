2b7f0f7cc74a9e15ea0a13622fef4b8b54db7b55
Tofan <tovan.kamil@gmail.com>
2025-03-13 23:17:38 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import Inspect from "vite-plugin-inspect";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), Inspect()],
 
   resolve: {
     alias: {
